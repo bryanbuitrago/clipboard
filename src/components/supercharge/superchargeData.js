9ce4e56b598d615730dbeaf1e795b760f86e4d8e
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 14:54:10 -0400

    Add  file with initial data.

---
+++ src/components/supercharge/superchargeData.js
@@ -0,0 +1,20 @@
+export const SUPERCHARGE_DATA = [
+  {
+    imgSrc: '/images/icon-blacklist.svg',
+    heading: 'Create Blacklists',
+    paragraph:
+      'Easily search your snippets by content, category, web address, application, and more.',
+  },
+  {
+    imgSrc: '/images/icon-text.svg',
+    heading: 'Plain Text Snippets',
+    paragraph:
+      'Remove unwanted formatting from copied text for a consistent look.',
+  },
+  {
+    imgSrc: '/images/icon-preview.svg',
+    heading: 'Sneak Preview',
+    paragraph:
+      'Quick preview of all snippets on your Clipboard for easy access.',
+  },
+];
