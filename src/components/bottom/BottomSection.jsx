65d4aff7c5301a2fe7117a792d0a5c4cd0dc2e7a
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 15:03:54 -0400

    Add BottomSection component.

---
+++ src/components/bottom/BottomSection.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+const BottomSection = () => {
+  return (
+    <section id='bottom'>
+      <div className='section-container my-20'>
+        <h3>clipboard for iOS and MacOS</h3>
+        <p className='section-content mb-10 text-xl'>
+          Available for free on the App Store. Download for Mac or iOS, sync
+          with iCloud and you're ready to start adding to your clipboard.
+        </p>
+        <div className='button-container'>
+          <Link
+            className='p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80'
+            href='#'
+          >
+            Download for iOS
+          </Link>
+          <Link
+            className='p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80'
+            href='#'
+          >
+            Download for Mac
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BottomSection;
