1b1f9226f06d359684548d3ae40f66ac55d7c231
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 14:35:25 -0400

    Add import statements for new components.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import FeaturesSection from '@/components/features/FeaturesSection';
 import HeroSection from '@/components/hero/HeroSection';
+import SnippetsSection from '@/components/snippets/SnippetsSection';
 
 export default function Home() {
   return (
     <main className='header-bg-image'>
       <HeroSection />
+      <SnippetsSection />
+      <FeaturesSection />
     </main>
   );
 }
