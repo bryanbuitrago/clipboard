9ed2dffbc1042716899a3af7394ef19a134b67ac
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 15:17:22 -0400

    Add footer data and social links.

---
+++ src/components/footer/footerData.js
@@ -0,0 +1,29 @@
+export const MENU_ITEMS_DATA = [
+  [
+    { title: 'FAQs', href: '#' },
+    { title: 'Contact Us', href: '#' },
+  ],
+  [
+    { title: 'Privacy Policy', href: '#' },
+    { title: 'Press Kit', href: '#' },
+  ],
+  [{ title: 'Install Guide', href: '#' }],
+];
+
+export const SOCIAL_LINKS_DATA = [
+  {
+    href: '#',
+    src: '/images/icon-facebook.svg',
+    alt: 'Facebook',
+  },
+  {
+    href: '#',
+    src: '/images/icon-twitter.svg',
+    alt: 'Twitter',
+  },
+  {
+    href: '#',
+    src: '/images/icon-instagram.svg',
+    alt: 'Instagram',
+  },
+];
