f1f8239c9e3ba2a704bd7af7b2d122e772eca749
bryanbuitrago <bryanbuitrago86@gmail.com>
2024-03-28 14:35:11 -0400

    Add features data.

---
+++ src/components/features/featuresData.js
@@ -0,0 +1,16 @@
+export const FEATURES_DATA = [
+  {
+    heading: 'Quick Search',
+    paragraph:
+      'Easily search your snippets by content, category, web address, application, and more.',
+  },
+  {
+    heading: 'iCloud Sync',
+    paragraph: 'Instantly saves and syncs snippets across all your devices.',
+  },
+  {
+    heading: 'Complete History',
+    paragraph:
+      'Retrieve any snippets from the first moment you started using the app.',
+  },
+];
